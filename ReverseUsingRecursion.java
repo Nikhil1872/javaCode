9c6db48da2273d9b6eac61c856052284f93e9592
Nikhil1872 <114892182+Nikhil1872@users.noreply.github.com>
2023-06-19 01:34:52 +0530

    Update ReverseUsingRecursion.java

--- ReverseUsingRecursion.java
+++ ReverseUsingRecursion.java
@@ -1,1 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		reverse(123);		 
+	}
+	static int rev=0;
+	static int m=0;
+ public static int reverse(int n)
+	 {
+	      m=n%10;
+	     rev=rev*10+m;
+	     if(n<10)
+	      return rev; 
+	     return recur(n/10);
+	   
+	 }
+}
 
