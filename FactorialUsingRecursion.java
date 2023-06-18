18eff7e791deb5b5beb681a41ff37d751ba31ea3
Nikhil1872 <114892182+Nikhil1872@users.noreply.github.com>
2023-06-19 01:51:14 +0530

    Update FactorialUsingRecursion.java

--- FactorialUsingRecursion.java
+++ FactorialUsingRecursion.java
@@ -1,1 +1,15 @@
-
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(recur(6));
+	}
+	static int fac=1;
+  public static int fact(int n)
+	 {
+	     fac=fac*n;
+	     if(n==1)
+	     return fac;
+	     return recur(n-1);
+	 }
+	 
+}
