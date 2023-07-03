291ae4a7b3601ee4983b3e5f589cac60ec86d524
Nikhil Talmale <114892182+Nikhil1872@users.noreply.github.com>
2023-07-03 18:10:59 +0530

    Update MergeString.java

--- MergeString.java
+++ MergeString.java
@@ -6,33 +6,37 @@
 
 //****************code**************************
 
-public class Main
-{
-	public static void main(String[] args) {
-		String hello1 ="ace";
-		String hello2 ="bdf";
-	        char [] arr1 = hello1.toCharArray();
-		char [] arr2 = hello2.toCharArray();
-		int len= arr1.length+arr2.length;
-		String str="";
-		int j=0;int k=0;
-		int i=0;
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
 
-	         while(i<len)
-		{
-		    if(j<arr1.length)
-		    {
-		        str=str+arr1[j];
-		        j++;
-		        i++;
-		   }
-		    if(k<arr2.length)
-		    {
-		       str=str+arr2[k];
-		       k++;
-		       i++;
-		    }
-		}
-	System.out.println(str);
+	String str="";
+	int l1=word1.length();
+	int l2=word2.length();
+	
+	if(l1>l2)
+	{
+            int i=0;
+	    for(i=0;i<l2;i++)
+	        str=str+word1.charAt(i)+word2.charAt(i);
+	    
+	    for(int j=i;j<l1;j++)
+	       str=str+word1.charAt(j);
+	}	
+	if(l1<l2)
+	{
+            int i=0;
+	    for(i=0;i<l1;i++)
+	        str=str+word1.charAt(i)+word2.charAt(i);
+	
+	    for(int j=i;j<l2;j++)
+	       str=str+word2.charAt(j);
 	}
+      if(l1==l2)
+      {
+       for(int i=0;i<l1;i++)
+	        str=str+word1.charAt(i)+word2.charAt(i);
+      }
+     return str;
+  
+    }
 }
